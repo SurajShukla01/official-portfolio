5a05c0e378ea9000709f181f1f16411926887f14
Suraj Shukla <shukla.suraj0101@gmail.com>
2025-03-20 05:43:51 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 100);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navlist.classList.toggle('open');
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
 };
 
 window.onscroll = () => {
-  menu.classList.remove('bx-x');
-  navlist.classList.remove('open');
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
 };
+
+function sendMail() {
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_tgbafls", "template_30r53ae", params)
+    .then(alert("Email Sent!"));
+}
